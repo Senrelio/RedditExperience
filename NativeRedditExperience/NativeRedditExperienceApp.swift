81db957a76bad3940e7d0e3cc6c859bcc6b70f5d
iwazaki <iwazaki@uta-senzen.com>
2020-07-10 15:36:25 +0800

    Initial Commit

---
+++ NativeRedditExperience/NativeRedditExperienceApp.swift
@@ -0,0 +1,17 @@
+//
+//  NativeRedditExperienceApp.swift
+//  NativeRedditExperience
+//
+//  Created by iwazaki on 7/10/20.
+//
+
+import SwiftUI
+
+@main
+struct NativeRedditExperienceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
